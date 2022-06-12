bfca4d34dd162134fae7fd73beee353c2ab5c419
jonathanavc <jonvenega@gmail.com>
2022-06-11 20:46:01 -0400

    aaa

--- code.py
+++ code.py
@@ -10,7 +10,7 @@ from collections import deque
 
 def create_q_model():
     # Network defined by the Deepmind paper
-    inputs = tf.keras.layers.Input(shape=(210, 160, 4))
+    inputs = tf.keras.layers.Input(shape=(84, 84, 4))
 
     # Convolutions on the frames on the screen
     layer1 = tf.keras.layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
@@ -31,13 +31,19 @@ ale.loadROM(Breakout)
 env = gym.make('ALE/Breakout-v5',        # Use all actions
     render_mode='human'                  # None | human | rgb_array
 )
-
 env = wrap_deepmind(env, frame_stack=True, scale=True)
 
-height, width, channels = env.observation_space.shape
+model = create_q_model()
+
+epsilon = 1.0
+epsilon_min = 0.1
+random_frames = 0
+epsilon_random = 0.9 / 1000000
+
+#height, width, channels = env.observation_space.shape
 actions = env.action_space.n
 env.unwrapped.get_action_meanings()
-max_steps = 1000;
+max_steps = 10000;
 episode = 0;
 while True:
     state = np.array(env.reset())
@@ -45,16 +51,26 @@ while True:
     episode += 1
     for step in range(1, max_steps):
         #time.sleep(0.1)
-        action = np.random.choice(actions)
+        if(step < random_frames or epsilon > np.random.rand(1)[0]):
+            action = np.random.choice(actions)
+        else:
+            # Predict action Q-values
+            # From environment state
+            state_tensor = tf.convert_to_tensor(state)
+            state_tensor = tf.expand_dims(state_tensor, 0)
+            action_probs = model(state_tensor, training=False)
+            # Take best action
+            action = tf.argmax(action_probs[0]).numpy()
         new_state, reward, done, info = env.step(action)
         score += reward
         new_state = np.array(new_state)
         memory.append((new_state, action, reward, state, done))
-        print(new_state.shape)
+        epsilon -= epsilon_random
+        epsilon = max(epsilon, epsilon_min)
+
         if done:
             break
-        
-    print('Episode:{} Score:{}'.format(episode, score))
+    print('Episode:{} Score:{} Epsilon:{}'.format(episode, score, epsilon))
     if(score > 40):
         break
 env.close()
