69351947e5e9aaadcdcd3870db2afc39df51ea2a
jonathanavc <jonvenega@gmail.com>
2022-06-11 22:47:59 -0400

    ta lito 😎

--- Nico culiao pollo.py
+++ Nico culiao pollo.py
@@ -1,5 +1,8 @@
+from math import gamma
 import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import time
 import gym
 from baselines.common.atari_wrappers import wrap_deepmind
@@ -10,41 +13,48 @@ from collections import deque
 
 def create_q_model():
     # Network defined by the Deepmind paper
-    inputs = tf.keras.layers.Input(shape=(84, 84, 4))
+    inputs = layers.Input(shape=(84, 84, 4))
 
     # Convolutions on the frames on the screen
-    layer1 = tf.keras.layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
-    layer2 = tf.keras.layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
-    layer3 = tf.keras.layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
+    layer1 = layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
+    layer2 = layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
+    layer3 = layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
 
-    layer4 = tf.keras.layers.Flatten()(layer3)
+    layer4 = layers.Flatten()(layer3)
 
-    layer5 = tf.keras.layers.Dense(512, activation="relu")(layer4)
+    layer5 = layers.Dense(512, activation="relu")(layer4)
     action = tf.keras.layers.Dense(4, activation="linear")(layer5)
 
-    return tf.keras.Model(inputs=inputs, outputs=action)
+    return keras.Model(inputs=inputs, outputs=action)
 
 memory = deque(maxlen = 100000)
 
 ale = ALEInterface()
 ale.loadROM(Breakout)
 env = gym.make('ALE/Breakout-v5',        # Use all actions
-    render_mode='human'                  # None | human | rgb_array
+    render_mode='rgb_array'                  # None | human | rgb_array
 )
 env = wrap_deepmind(env, frame_stack=True, scale=True)
 
 model = create_q_model()
+model_target = create_q_model()
+optimizer = keras.optimizers.Adam(learning_rate=0.00025, clipnorm=1.0)
+loss_function = keras.losses.Huber()
 
 epsilon = 1.0
 epsilon_min = 0.1
-random_frames = 0
-epsilon_random = 0.9 / 1000000
+random_frames = 100000
+delta_epsilon = 0.9 / 100000
+
+batch_size = 32
+gamma = 0.99
 
 #height, width, channels = env.observation_space.shape
 actions = env.action_space.n
 env.unwrapped.get_action_meanings()
-max_steps = 10000;
-episode = 0;
+max_steps = 10000
+episode = 0
+max_score = 0
 while True:
     state = np.array(env.reset())
     score = 0
@@ -53,8 +63,7 @@ while True:
         #time.sleep(0.1)
         if(step < random_frames or epsilon > np.random.rand(1)[0]):
             action = np.random.choice(actions)
-            
-        else:
+        else:    
             # Predict action Q-values
             # From environment state
             state_tensor = tf.convert_to_tensor(state)
@@ -66,14 +75,58 @@ while True:
         new_state, reward, done, info = env.step(action)
         score += reward
         new_state = np.array(new_state)
-        memory.append((new_state, action, reward, state, done))
-        epsilon -= epsilon_random
+        memory.append((state, new_state, action, reward, done))
+        epsilon -= delta_epsilon
         epsilon = max(epsilon, epsilon_min)
+
+
+        if(step%4 == 0 and len(memory) >= batch_size):
+            indices = np.random.choice(range(len(memory)), size = batch_size)
+            
+            # Using list comprehension to sample from replay buffer
+            state_sample = np.array([memory[i][0] for i in indices])
+            state_next_sample = np.array([memory[i][1] for i in indices])
+            action_sample = [memory[i][2] for i in indices]
+            rewards_sample = [memory[i][3] for i in indices]
+            done_sample = tf.convert_to_tensor(
+                [float(memory[i][4]) for i in indices]
+            )
+
+            # Build the updated Q-values for the sampled future states
+            # Use the target model for stability
+            future_rewards = model_target.predict(state_next_sample)
+            # Q value = reward + discount factor * expected future reward
+            updated_q_values = rewards_sample + gamma * tf.reduce_max(
+                future_rewards, axis=1
+            )
+
+            # If final frame set the last value to -1
+            updated_q_values = updated_q_values * (1 - done_sample) - done_sample
+
+            # Create a mask so we only calculate loss on the updated Q-values
+            masks = tf.one_hot(action_sample, actions)
+
+            with tf.GradientTape() as tape:
+                # Train the model on the states and updated Q-values
+                q_values = model(state_sample)
+
+                # Apply the masks to the Q-values to get the Q-value for action taken
+                q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+                # Calculate loss between new Q-value and old Q-value
+                loss = loss_function(updated_q_values, q_action)
+
+            # Backpropagation
+            grads = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
 
         if done:
             break
-        
+    
     print('Episode:{} Score:{} Epsilon:{}'.format(episode, score, epsilon))
+    if(score > max_score):
+        model.save_weights('weights/Episode_{}_Score_{}.h5'.format(episode,score))
+        max_score = score
     if(score > 40):
         break
 
---
+++ try.py
@@ -0,0 +1,63 @@
+from math import gamma
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import time
+import gym
+from baselines.common.atari_wrappers import wrap_deepmind
+from ale_py import ALEInterface
+from ale_py.roms import Breakout
+from collections import deque
+
+
+def create_q_model():
+    # Network defined by the Deepmind paper
+    inputs = layers.Input(shape=(84, 84, 4))
+
+    # Convolutions on the frames on the screen
+    layer1 = layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
+    layer2 = layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
+    layer3 = layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
+
+    layer4 = layers.Flatten()(layer3)
+
+    layer5 = layers.Dense(512, activation="relu")(layer4)
+    action = tf.keras.layers.Dense(4, activation="linear")(layer5)
+
+    return keras.Model(inputs=inputs, outputs=action)
+
+memory = deque(maxlen = 100000)
+
+ale = ALEInterface()
+ale.loadROM(Breakout)
+env = gym.make('ALE/Breakout-v5',        # Use all actions
+    render_mode='human'                  # None | human | rgb_array
+)
+env = wrap_deepmind(env, frame_stack=True, scale=True)
+
+model = create_q_model()
+model.load_weights('weights\Episode_11_Score_2.0.h5')
+
+#height, width, channels = env.observation_space.shape
+actions = env.action_space.n
+env.unwrapped.get_action_meanings()
+max_steps = 10000
+episode = 0
+while True:
+    state = np.array(env.reset())
+    score = 0
+    episode += 1
+    for step in range(1, max_steps):
+        state_tensor = tf.convert_to_tensor(state)
+        state_tensor = tf.expand_dims(state_tensor, 0)
+        action_probs = model(state_tensor, training=False)
+        action = tf.argmax(action_probs[0]).numpy()
+        new_state, reward, done, info = env.step(action)
+        score += reward
+        new_state = np.array(new_state)
+        memory.append((state, new_state, action, reward, done))
+
+    print('Episode:{} Score:{}'.format(episode, score))
+
+env.close()
