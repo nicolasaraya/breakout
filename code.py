cfce731cf48d08f11ac898a52412fb0f6d2d243e
jonathanavc <jonvenega@gmail.com>
2022-06-11 20:22:10 -0400

    a

--- code.py
+++ code.py
@@ -1,31 +1,29 @@
+import numpy as np
+import tensorflow as tf
+import time
+import gym
 from baselines.common.atari_wrappers import wrap_deepmind
 from ale_py import ALEInterface
 from ale_py.roms import Breakout
-import time
-import gym
-import numpy as np
 from collections import deque
-'''''
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+
 
 def create_q_model():
     # Network defined by the Deepmind paper
-    inputs = layers.Input(shape=(210, 160, 4))
+    inputs = tf.keras.layers.Input(shape=(210, 160, 4))
 
     # Convolutions on the frames on the screen
-    layer1 = layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
-    layer2 = layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
-    layer3 = layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
+    layer1 = tf.keras.layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
+    layer2 = tf.keras.layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
+    layer3 = tf.keras.layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
 
-    layer4 = layers.Flatten()(layer3)
+    layer4 = tf.keras.layers.Flatten()(layer3)
 
-    layer5 = layers.Dense(512, activation="relu")(layer4)
-    action = layers.Dense(4, activation="linear")(layer5)
+    layer5 = tf.keras.layers.Dense(512, activation="relu")(layer4)
+    action = tf.keras.layers.Dense(4, activation="linear")(layer5)
 
-    return keras.Model(inputs=inputs, outputs=action)
-'''
+    return tf.keras.Model(inputs=inputs, outputs=action)
+
 memory = deque(maxlen = 100000)
 
 ale = ALEInterface()
