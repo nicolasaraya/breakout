7d26c40c4840bcc290b4d2974d098a185e41fab6
Nicolas Araya <naraya2018@inf.udec.cl>
2022-07-12 20:46:21 -0400

    update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import tensorflow.compat.v1 as tfc
 import keras
 import datetime
 import gym.wrappers
+import argparse
 from other.PreprocessAtari import PreprocessAtari
 from other.FrameBuffer import FrameBuffer
 from other.DQNAgent import DQNAgent
@@ -21,18 +22,30 @@ from tqdm import trange
 from IPython.display import clear_output
 from pandas import DataFrame
 
+def parse():
+    parser = argparse.ArgumentParser(description="main")
+    parser.add_argument('--train_dqn', type=int, nargs = '?',const=10000, default = None,  help = 'Train model with DQN')
+    parser.add_argument('--test_dqn', action = 'store_true',  help = 'Test model')
+    parser.add_argument('--render', action = 'store_true', default = None, help = 'Render model')
+    args = parser.parse_args()
+    return args
 
-def make_env():
+def make_env(render = False):
     ale = ALEInterface()
     ale.loadROM(Breakout)
-    env = gym.make('ALE/Breakout-v5',        # Use all actions
-        render_mode='rgb_array'                 # None | human | rgb_array
-    )
+    if(render):
+        env = gym.make('ALE/Breakout-v5',        # Use all actions
+            render_mode='human'                 # None | human | rgb_array
+        )
+    else: 
+        env = gym.make('ALE/Breakout-v5',        # Use all actions
+                render_mode='rgb_array'                 # None | human | rgb_array
+            )
     env = PreprocessAtari(env)
     env = FrameBuffer(env, n_frames=4, dim_order='tensorflow')
     return env
 
-def evaluate(env, agent, n_games=1, greedy=False, t_max=10000):
+def evaluate(env, agent, n_games=1, greedy=False, t_max=10000, render = None):
     """ Plays n_games full games. If greedy, picks actions as argmax(qvalues). Returns mean reward. """
     rewards = []
     for _ in range(n_games):
@@ -41,11 +54,13 @@ def evaluate(env, agent, n_games=1, greedy=False, t_max=10000):
         for _ in range(t_max):
             qvalues = agent.get_qvalues([s])
             action = qvalues.argmax(axis=-1)[0] if greedy else agent.sample_actions(qvalues)[0]
+            if render: 
+                env.render(mode= 'rgb_array')
             s, r, done, _ = env.step(action)
             reward += r
             if done: 
                 break
-                
+            
         rewards.append(reward)
     return np.mean(rewards)
 
@@ -85,127 +100,158 @@ def load_weigths_into_target_network(agent, target_network):
         assigns.append(tfc.assign(w_target, w_agent, validate_shape=True))
     tfc.get_default_session().run(assigns)
 
-def sample_batch(exp_replay, batch_size):
+
+def sample_batch(exp_replay, batch_size, obs_ph, actions_ph, rewards_ph, next_obs_ph, is_done_ph):
     obs_batch, act_batch, reward_batch, next_obs_batch, is_done_batch = exp_replay.sample(batch_size)
-    return {
-        obs_ph:obs_batch, actions_ph:act_batch, rewards_ph:reward_batch, 
-        next_obs_ph:next_obs_batch, is_done_ph:is_done_batch
+    return { 
+        obs_ph:obs_batch, actions_ph:act_batch, rewards_ph:reward_batch,  next_obs_ph:next_obs_batch, is_done_ph:is_done_batch
     }
 
-env = make_env()
-env.reset()
-state_dim = env.observation_space.shape
-n_actions = env.action_space.n
+def train(args):
+    env = make_env()
+    env.reset()
+    state_dim = env.observation_space.shape
+    n_actions = env.action_space.n
 
-'''
-for _ in range(50):
-    obs, _, _, _ = env.step(env.action_space.sample())
+    '''
+    for _ in range(50):
+        obs, _, _, _ = env.step(env.action_space.sample())
 
-plt.title("Game image")
-plt.imshow(env.render(mode = "rgb_array"))
-plt.show()
-plt.title("Agent observation (4 frames left to right)")
-plt.imshow(obs.transpose([0,2,1]).reshape([state_dim[0],-1]));
-plt.show()
-'''
-##network 
-tfc.reset_default_graph()
-sess = tfc.InteractiveSession()
+    plt.title("Game image")
+    plt.imshow(env.render(mode = "rgb_array"))
+    plt.show()
+    plt.title("Agent observation (4 frames left to right)")
+    plt.imshow(obs.transpose([0,2,1]).reshape([state_dim[0],-1]));
+    plt.show()
+    '''
 
-agent = DQNAgent("dqn_agent", state_dim, n_actions, epsilon=0.5)
-sess.run(tfc.global_variables_initializer())
-##agente sin entrenar
-print (evaluate(env, agent, n_games=1))
-##experiencia
-target_network = DQNAgent("target_network", state_dim, n_actions)
-load_weigths_into_target_network(agent, target_network) 
-sess.run([tf.assert_equal(w, w_target) for w, w_target in zip(agent.weights, target_network.weights)]);
+    ##network 
+    tfc.reset_default_graph()
+    sess = tfc.InteractiveSession()
 
-obs_ph = tfc.placeholder(tf.float32, shape=(None,) + state_dim)
-actions_ph = tfc.placeholder(tf.int32, shape=[None])
-rewards_ph = tfc.placeholder(tf.float32, shape=[None])
-next_obs_ph = tfc.placeholder(tf.float32, shape=(None,) + state_dim)
-is_done_ph = tfc.placeholder(tf.float32, shape=[None])
+    agent = DQNAgent("dqn_agent", state_dim, n_actions, epsilon=0.5)
+    sess.run(tfc.global_variables_initializer())
+    ##agente sin entrenar
+    print (evaluate(env, agent, n_games=1))
+    ##experiencia
+    target_network = DQNAgent("target_network", state_dim, n_actions)
+    load_weigths_into_target_network(agent, target_network) 
+    sess.run([tf.assert_equal(w, w_target) for w, w_target in zip(agent.weights, target_network.weights)]);
 
-is_not_done = 1 - is_done_ph
-gamma = 0.99
+    obs_ph = tfc.placeholder(tf.float32, shape=(None,) + state_dim)
+    actions_ph = tfc.placeholder(tf.int32, shape=[None])
+    rewards_ph = tfc.placeholder(tf.float32, shape=[None])
+    next_obs_ph = tfc.placeholder(tf.float32, shape=(None,) + state_dim)
+    is_done_ph = tfc.placeholder(tf.float32, shape=[None])
 
-current_qvalues = agent.get_symbolic_qvalues(obs_ph, n_actions)
-current_action_qvalues = tf.reduce_sum(tf.one_hot(actions_ph, n_actions) * current_qvalues, axis=1)
+    is_not_done = 1 - is_done_ph
+    gamma = 0.99
 
-# compute q-values for NEXT states with target network
-next_qvalues_target = target_network.get_symbolic_qvalues(next_obs_ph, n_actions) #<your code> 
+    current_qvalues = agent.get_symbolic_qvalues(obs_ph, n_actions)
+    current_action_qvalues = tf.reduce_sum(tf.one_hot(actions_ph, n_actions) * current_qvalues, axis=1)
 
-# compute state values by taking max over next_qvalues_target for all actions
-next_state_values_target = tf.reduce_max(next_qvalues_target, axis=1) #<YOUR CODE>
+    # compute q-values for NEXT states with target network
+    next_qvalues_target = target_network.get_symbolic_qvalues(next_obs_ph, n_actions) #<your code> 
 
-# compute Q_reference(s,a) as per formula above.
-reference_qvalues = rewards_ph + gamma * next_state_values_target #<YOUR CODE>
+    # compute state values by taking max over next_qvalues_target for all actions
+    next_state_values_target = tf.reduce_max(next_qvalues_target, axis=1) #<YOUR CODE>
 
-# Define loss function for sgd.
-td_loss = (current_action_qvalues - reference_qvalues) ** 2
-td_loss = tf.reduce_mean(td_loss)
+    # compute Q_reference(s,a) as per formula above.
+    reference_qvalues = rewards_ph + gamma * next_state_values_target #<YOUR CODE>
 
-train_step = tfc.train.AdamOptimizer(1e-3).minimize(td_loss, var_list=agent.weights)
+    # Define loss function for sgd.
+    td_loss = (current_action_qvalues - reference_qvalues) ** 2
+    td_loss = tf.reduce_mean(td_loss)
 
-sess.run(tfc.global_variables_initializer())
+    train_step = tfc.train.AdamOptimizer(1e-3).minimize(td_loss, var_list=agent.weights)
 
-mean_rw_history = []
-td_loss_history = []
+    sess.run(tfc.global_variables_initializer())
 
-exp_replay = ReplayBuffer(10**4)
-play_and_record(agent, env, exp_replay, n_steps=10000)
-moving_average = lambda x, span, **kw: DataFrame({'x':np.asarray(x)}).x.ewm(span=span, **kw).mean().values
-num_episodes = 10**6
+    mean_rw_history = []
+    td_loss_history = []
 
-agent.epsilon = 1
-
-for i in trange(num_episodes):
-    # play
-    play_and_record(agent, env, exp_replay, 10)
-    
-    # train
-    _, loss_t = sess.run([train_step, td_loss], sample_batch(exp_replay, batch_size=64))
-    td_loss_history.append(loss_t)
-    
-    # adjust agent parameters
-    if i % 500 == 0:
-        load_weigths_into_target_network(agent, target_network)
-        agent.epsilon = max(agent.epsilon - (agent.epsilon/(( num_episodes*0.9 ) / 500)), 0.01)
-        mean_rw_history.append(evaluate(make_env(), agent, n_games=3))
-    
-    if i % 1000 == 0:
-        #if i == 0: 
-            #continue
-        s = datetime.datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
-        clear_output(True)
-        print("buffer size = %i, epsilon = %.5f" % (len(exp_replay), agent.epsilon))
+    exp_replay = ReplayBuffer(10**5)
+    play_and_record(agent, env, exp_replay, n_steps=10000)
+    moving_average = lambda x, span, **kw: DataFrame({'x':np.asarray(x)}).x.ewm(span=span, **kw).mean().values
+    num_episodes = args.train_dqn
+    agent.epsilon = 1
+    print("Training with %i episodes"  %num_episodes)
+    for i in trange(num_episodes): #trange muestra progreso en %
+        # play
+        play_and_record(agent, env, exp_replay, 10)
         
-
-        fig, (plt1, plt2) = plt.subplots(2)
-        fig.suptitle('Breakout training')
-        plt1.set_title("Mean reward per game")
-        plt1.plot(mean_rw_history, 'tab:blue')
-        plt1.set(xlabel='Episode', ylabel  = 'Reward')
-        plt1.grid()
-        plt2.set_title("TD loss history (moving average)")
-        plt2.plot(moving_average(np.array(td_loss_history), span=100, min_periods=100), 'tab:red')
-        plt2.set(xlabel='Movement', ylabel  = 'Value')
-        plt2.grid()
-        fig.tight_layout()
-        fig.savefig('./imgs/graphic_2_{}.png'.format(s))
-        fig.clear()
-    #if np.mean(mean_rw_history[-10:]) > 10.:
-    #    break
-
-    if i % 10000 == 0:
-        if i != 0 : 
+        # train
+        _, loss_t = sess.run([train_step, td_loss], sample_batch(exp_replay, 
+                                                            batch_size=64, 
+                                                            obs_ph=obs_ph, 
+                                                            actions_ph=actions_ph,
+                                                            rewards_ph=rewards_ph, 
+                                                            next_obs_ph=next_obs_ph, 
+                                                            is_done_ph=is_done_ph))
+        td_loss_history.append(loss_t)
+        
+        # adjust agent parameters
+        if i % 500 == 0:
+            load_weigths_into_target_network(agent, target_network)
+            agent.epsilon = max(agent.epsilon - ( agent.epsilon/(num_episodes/ (500*1.2)) ), 0.01)
+            mean_rw_history.append(evaluate(make_env(), agent, n_games=3))
+        
+        if i % 1000 == 0:
+            #if i == 0: 
+                #continue
             s = datetime.datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
-            agent.network.save_weights('./model/weights_{}.h5'.format(s))
+            clear_output(True)
+            print("buffer size = %i, epsilon = %.5f" % (len(exp_replay), agent.epsilon))
+            
+            fig, (plt1, plt2) = plt.subplots(2)
+            fig.suptitle('Breakout training')
+            plt1.set_title("Mean reward per game")
+            plt1.plot(mean_rw_history, 'tab:blue')
+            plt1.set(xlabel='Games', ylabel  = 'Reward')
+            plt1.grid()
+            plt2.set_title("TD loss history (moving average)")
+            plt2.plot(moving_average(np.array(td_loss_history), span=100, min_periods=100), 'tab:red')
+            plt2.set(xlabel='Episodes', ylabel  = 'Value')
+            plt2.grid()
+            fig.tight_layout()
+            fig.savefig('./imgs/graphic_2_{}.png'.format(s))
+            fig.clear()
+        #if np.mean(mean_rw_history[-10:]) > 10.:
+        #    break
+        if i % 10000 == 0 or i == num_episodes-1:
+            if i != 0 : 
+                s = datetime.datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
+                agent.network.save_weights('./model/weights_{}.h5'.format(s + "eps_" + str(agent.epsilon)))
 
-#plt.show
+def test(args):
+    files = [f for f in os.listdir("./model") if os.path.isfile(os.path.join("./model", f))]
+    x = files[len(files)-1] #ultimo peso
+    path = "./model/" + x
 
-##demo
+    print("Model path: ")
+    print(path)
+    env = make_env()
+    if args.render:
+        env = make_env(render = True)
+    env.reset()
+    n_actions = env.action_space.n
+    state_dim = env.observation_space.shape
 
+    ##network 
+    tfc.reset_default_graph()
+    sess = tfc.InteractiveSession()
+    agent = DQNAgent("dqn_agent", state_dim, n_actions, epsilon=0)
+    agent.network.load_weights(path)
+    agent.epsilon = 0
+    n_games = 1
+    sess.run(tfc.global_variables_initializer())
+    result = evaluate(env, agent, n_games=n_games, render=args.render)
+    print ("Reward mean: %i , games = %i" % (result,n_games))
+    
 
-agent.epsilon=0
+if __name__ == '__main__':
+    args = parse()
+    if args.train_dqn:
+        train(args)
+    if args.test_dqn:
+        test(args)
--- readme.md
+++ readme.md
@@ -1,6 +1,14 @@
 # Breakout
 
-**requirements:**
+## Usage
+* **Train DQN** 
+    -`python main.py --test_dqn`  with 10000 episodes
+    -`python main.py --test_dqn N`  with N episodes
+* **Test Agent**
+    -`python main.py --train_dqn` train without render
+    -`python main.py --train_dqn --render` train with render
+
+## requirements:
 - VS Build Toolkit (windows)
 - pip install tensorflow
 - pip install ale-py
@@ -12,4 +20,4 @@
 - pip install matplot
 - pip install tqdm
 - pip instal IPython
-- ale-import-roms roms/ 
+- `ale-import-roms roms`
