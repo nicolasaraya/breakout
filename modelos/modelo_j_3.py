961ce94db37391a5e7140475b0671d9b1d70bcc6
jonathanavc <jonvenega@gmail.com>
2022-07-18 19:55:59 -0400

    🥸☝️

--- modelos/modelo_j_3.py
+++ modelos/modelo_j_3.py
@@ -1,31 +1,31 @@
-seed = 42
+max_episodes = 10000
 gamma = 0.99
-batch_size = 256 #256 max
-max_steps = 30000
+batch_size = 32 #256 max
+max_steps = 10000
 memory_size = 100000
-learning_rate = 0.0005
+learning_rate = 1e-3
 epsilon = 1.0
 epsilon_min = 0.1
-random_episodes = 2000
-update_after_actions = 16
-update_q_after_episodes = 200
-episodes_to_epsilon_min = 8000
+random_episodes = 0
+update_after_episodes = 1
+update_q_after_episodes = 10
+episodes_to_epsilon_min = 5000
 delta_epsilon = (epsilon - epsilon_min) / episodes_to_epsilon_min
 loss_function = keras.losses.Huber()
 optimizer = keras.optimizers.Adam(learning_rate= learning_rate)
 
 def create_q_model():
     # Network defined by the Deepmind paper
-    inputs = layers.Input(shape=(84, 84, 4))
+    inputs = layers.Input(shape=(64, 64, 4))
 
     # Convolutions on the frames on the screen
-    layer1 = layers.Conv2D(32,(8,8), strides=2, activation="relu")(inputs)
-    layer2 = layers.Conv2D(64,(4,4), strides=2, activation="relu")(layer1)
-    layer3 = layers.Conv2D(64,(2,2), strides=1, activation="relu")(layer2)
+    layer1 = layers.Conv2D(filters=16, kernel_size=(3, 3), strides=(2, 2), activation='relu')(inputs)
+    layer2 = layers.Conv2D(filters=32, kernel_size=(3, 3), strides=(2, 2), activation='relu')(layer1)
+    layer3 = layers.Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu')(layer2)
 
     layer4 = layers.Flatten()(layer3)
 
-    layer5 = layers.Dense(1024, activation="relu")(layer4)
+    layer5 = layers.Dense(256, activation='relu')(layer4)
     action = tf.keras.layers.Dense(4, activation="linear")(layer5)
 
     return keras.Model(inputs=inputs, outputs=action)
--- train.py
+++ train.py
@@ -5,6 +5,9 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import time
 import gym
+from PreprocessAtari import PreprocessAtari
+from FrameBuffer import FrameBuffer
+from tqdm import trange
 from baselines.common.atari_wrappers import wrap_deepmind
 from ale_py import ALEInterface
 from ale_py.roms import Breakout
@@ -17,9 +20,9 @@ MODELO = 'modelo_j_3'
 
 codigo_modelo = glob.glob('./modelos/{}.py'.format(MODELO))
 for linea_de_codigo in codigo_modelo: 
-     o = open(linea_de_codigo)  
-     r = o.read()       
-     exec(r)
+    o = open(linea_de_codigo)
+    r = o.read()       
+    exec(r)
 
 memory = deque(maxlen = memory_size)
 
@@ -28,24 +31,22 @@ ale.loadROM(Breakout)
 env = gym.make('ALE/Breakout-v5',           # Use all actions
     render_mode='rgb_array'                 # None | human | rgb_array
 )
-env = wrap_deepmind(env, frame_stack=True, scale=True)
-env.seed(seed)
+env = PreprocessAtari(env)
+env = FrameBuffer(env ,n_frames=4, dim_order='tensorflow')
 
 model = create_q_model()
 model_target = create_q_model()
 
 actions = env.action_space.n
 
-episode = 0
 score_prom = 0
 score_prom_history = []
 episode_history = []
 max_score_prom = 0
 
-while True:
+for episode in trange(max_episodes):
     state = np.array(env.reset())
     score = 0
-    episode += 1
     if(episode > random_episodes):
         epsilon -= delta_epsilon
         epsilon = max(epsilon, epsilon_min)
@@ -70,56 +71,58 @@ while True:
         memory.append((state, new_state, action, reward, done))
         state = new_state
 
-        if(step % update_after_actions == 0 and len(memory) >= batch_size):
-            indices = np.random.choice(range(len(memory)), size = batch_size)
-            # Using list comprehension to sample from replay buffer
-            state_sample = np.array([memory[i][0] for i in indices])
-            state_next_sample = np.array([memory[i][1] for i in indices])
-            action_sample = [memory[i][2] for i in indices]
-            rewards_sample = [memory[i][3] for i in indices]
-            done_sample = tf.convert_to_tensor(
-                [float(memory[i][4]) for i in indices]
-            )
-            # Build the updated Q-values for the sampled future states
-            # Use the target model for stability
-            future_rewards = model_target.predict(state_next_sample, verbose = 0)
-            # Q value = reward + discount factor * expected future reward
-            updated_q_values = rewards_sample + gamma * tf.reduce_max(
-                future_rewards, axis=1
-            )
-
-            # If final frame set the last value to -1
-            updated_q_values = updated_q_values * (1 - done_sample) - done_sample
-
-            # Create a mask so we only calculate loss on the updated Q-values
-            masks = tf.one_hot(action_sample, actions)
-
-            with tf.GradientTape() as tape:
-                # Train the model on the states and updated Q-values
-                q_values = model(state_sample)
-
-                # Apply the masks to the Q-values to get the Q-value for action taken
-                q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
-                # Calculate loss between new Q-value and old Q-value
-                loss = loss_function(updated_q_values, q_action)
-
-            # Backpropagation
-            grads = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(grads, model.trainable_variables))
-            
         if done:
             break
+
+    if(step % update_after_episodes == 0 and len(memory) >= batch_size):
+        indices = np.random.choice(range(len(memory)), size = batch_size)
+        # Using list comprehension to sample from replay buffer
+        state_sample = np.array([memory[i][0] for i in indices])
+        state_next_sample = np.array([memory[i][1] for i in indices])
+        action_sample = [memory[i][2] for i in indices]
+        rewards_sample = [memory[i][3] for i in indices]
+        done_sample = tf.convert_to_tensor(
+            [float(memory[i][4]) for i in indices]
+        )
+        # Build the updated Q-values for the sampled future states
+        # Use the target model for stability
+        future_rewards = model_target.predict(state_next_sample, verbose = 0)
+        # Q value = reward + discount factor * expected future reward
+        updated_q_values = rewards_sample + gamma * tf.reduce_max(
+            future_rewards, axis=1
+        )
+
+        # If final frame set the last value to -1
+        updated_q_values = updated_q_values * (1 - done_sample) - done_sample
+
+        # Create a mask so we only calculate loss on the updated Q-values
+        masks = tf.one_hot(action_sample, actions)
+
+        with tf.GradientTape() as tape:
+            # Train the model on the states and updated Q-values
+            q_values = model(state_sample)
+
+            # Apply the masks to the Q-values to get the Q-value for action taken
+            q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+            # Calculate loss between new Q-value and old Q-value
+            loss = loss_function(updated_q_values, q_action)
+
+        # Backpropagation
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            
+        
     
     # print()
     score_prom += score/10.0
     if(episode > 9 and episode%10 == 0):
-        print('Episode:{} Score:{} Epsilon:{}'.format(episode, round(score_prom, 2), round(epsilon,3)))
         score_prom_history.append( score_prom )
         episode_history.append(episode)
         if(score_prom > max_score_prom):
-            max_score_prom = score_prom
+            #print('Episode:{} Score:{} Epsilon:{}'.format(episode, round(score_prom, 2), round(epsilon,3)))
+            #print('____________SAVE____________')
             model.save_weights('modelos/weights/{}.h5'.format(MODELO,episode,round(score_prom, 2)))
-            print('____________SAVE____________')
+            max_score_prom = score_prom
         score_prom = 0
 
     #save graph
@@ -129,9 +132,8 @@ while True:
         plt.savefig('modelos/graphs/{}.png'.format(MODELO,episode,round(score_prom, 2)))
 
     # update the the target network with new weights
-    if(episode>= update_q_after_episodes):
+    if(episode > 0 and episode % update_q_after_episodes == 0):
         model_target.set_weights(model.get_weights())
-
     if(score > 40):
         break
 
